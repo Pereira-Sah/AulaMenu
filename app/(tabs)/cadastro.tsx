7767243caf5cc85075ef7919bb0865e28b4c00b6
pereira-sah <sabrina.pereirasantos1105@gmail.com>
2026-03-23 15:52:00 -0300

    Api Usuario

--- app/(tabs)/cadastro.tsx
+++ app/(tabs)/cadastro.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { useState, useEffect} from 'react';
 export default function App() {
+
+  const [api, setApi] = useState()
+
+    async function buscarApi(){
+      const dados = await fetch('https://jsonplaceholder.typicode.com/users')
+      const json = await dados.json()
+      setApi(json)
+    }
+
+    useEffect(()=>{
+      buscarApi()
+    },[])
   return (
     <View style={styles.container}>
-      <Text>Cadastro</Text>
+      <Text>Usuarios</Text>
+      <FlatList
+       data={api}
+       keyExtractor={item=>item.id}
+       renderItem={({item})=><Text>{item.name}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
